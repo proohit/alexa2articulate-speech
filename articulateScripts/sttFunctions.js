7fb2415eab73252c9784fb7edcb038ab5c1031db
proohit <direnc99@gmail.com>
2021-12-03 18:19:00 +0100

    add timeout to reset recognition, add intermin result

--- articulateScripts/sttFunctions.js
+++ articulateScripts/sttFunctions.js
@@ -1,59 +1,75 @@
 let recognition = null;
+let player = GetPlayer();
+
+setTimeout(() => {
+  if (player.GetVar("sttEnabled") && recognition !== null) {
+    recognition.abort();
+  }
+}, 15000);
 
 function startSTT() {
-    console.log("Start STT");
-    let player = GetPlayer();
-    let finalTranscript = '';
+  console.log("Start STT");
+  let finalTranscript = "";
 
-    if (!('webkitSpeechRecognition' in window)) {
-    } else if (recognition == null) {
-        recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.lang = 'de-de';
-        recognition.continuous = true;
-        recognition.interimResults = true;
+  if (!("webkitSpeechRecognition" in window)) {
+  } else if (recognition == null) {
+    recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.lang = "de-de";
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-        recognition.onresult = function (event) {
-            var interim_transcript = '';
+    recognition.onresult = function (event) {
+      var interim_transcript = "";
 
-            for (var i = event.resultIndex; i < event.results.length; ++i) {
-                if (event.results[i].isFinal) {
-                    finalTranscript += event.results[i][0].transcript;
-                } else {
-                    interim_transcript += event.results[i][0].transcript;
-                }
-            }
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          finalTranscript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+      if (interim_transcript) {
+        player.SetVar("spokenText", interim_transcript);
+      }
 
-            if (finalTranscript) {
-                try {
-                    finalTranscript = finalTranscript.toLowerCase();
-                    console.log(finalTranscript);
-                    const res = peg$parse(finalTranscript);
-                    console.log(res);
-                    res.execute();
-                } finally {
-                    finalTranscript = '';
-                }
-            }
-        };
+      if (finalTranscript) {
+        try {
+          finalTranscript = finalTranscript.toLowerCase();
+          console.log(finalTranscript);
+          const res = peg$parse(finalTranscript);
+          console.log(res);
+          res.execute();
+        } finally {
+          finalTranscript = "";
+        }
+      }
+    };
 
-        recognition.onerror = function (event) {
-            console.log(event);
-            //This prevents stopping of the recognition
-            player.GetVar('sttEnabled') ? recognition.start() : stopSTT();
-        };
-        
-        recognition.onend = function (event) {
-            //This prevents stopping of the recognition
-            player.GetVar('sttEnabled') ? recognition.start() : stopSTT();
-        };
-
+    recognition.onerror = function (event) {
+      console.log(event);
+      //This prevents stopping of the recognition
+      recognition.abort();
+      if (player.GetVar("sttEnabled")) {
         recognition.start();
-    }
+      }
+    };
+
+    recognition.onend = function (event) {
+      console.log(event);
+      //This prevents stopping of the recognition
+      if (player.GetVar("sttEnabled")) {
+        recognition.start();
+      } else {
+        stopSTT();
+      }
+    };
+    recognition.start();
+  }
 }
 
 function stopSTT() {
-    console.log("Stop STT");
-    recognition.abort();
-    recognition = null;
+  console.log("Stop STT");
+  recognition.abort();
+  recognition = null;
 }
