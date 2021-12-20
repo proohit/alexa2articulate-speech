9642d50e129b12d7d01f3c28fe71516b04ace209
Andreas Sauerwein <an.srwn@gmail.com>
2021-12-20 02:06:46 +0100

    visualize speaking and spacebar activation

--- articulateScripts/sttFunctions.js
+++ articulateScripts/sttFunctions.js
@@ -1,11 +1,30 @@
 let recognition = null;
 let player = GetPlayer();
+let spaceBarPressed = false;
 
 setInterval(() => {
   if (player.GetVar("sttEnabled") && recognition !== null) {
-    recognition.abort();
+    recognition.stop();
   }
 }, 15000);
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === "Space") {
+    if (!spaceBarPressed) {
+      spaceBarPressed = true;
+      player.SetVar("sttEnabled", true);
+      player.SetVar("sttIsActive", true);
+    }
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === "Space") {
+    spaceBarPressed = false;
+    player.SetVar("sttIsActive", false);
+    player.SetVar("sttEnabled", false);
+  }
+});
 
 function startSTT() {
   console.log("Start STT");
@@ -20,31 +39,37 @@ function startSTT() {
     recognition.interimResults = true;
 
     recognition.onresult = function (event) {
-      var interim_transcript = "";
+      const assistantName = player.GetVar("assistantName").toLowerCase();
+      var interimTranscript = "";
 
       for (var i = event.resultIndex; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
           finalTranscript += event.results[i][0].transcript;
         } else {
-          interim_transcript += event.results[i][0].transcript;
+          interimTranscript += event.results[i][0].transcript;
         }
       }
-      if (interim_transcript) {
-        player.SetVar("spokenText", interim_transcript);
+
+      if (interimTranscript && interimTranscript.toLowerCase().includes(assistantName)) {
+        player.SetVar("sttIsActive", true);
+        player.SetVar("spokenText", interimTranscript);
       }
 
       if (finalTranscript) {
-        player.SetVar("spokenText", finalTranscript);
         try {
-          finalTranscript = finalTranscript.toLowerCase();
-          finalTranscript = removePunctuation(finalTranscript);
-          console.log(finalTranscript);
-          const res = peg$parse(finalTranscript);
-          console.log(res);
-          res.execute();
+          if (finalTranscript.toLowerCase().includes(assistantName)) {
+            player.SetVar("spokenText", finalTranscript);
+            finalTranscript = finalTranscript.toLowerCase();
+            finalTranscript = removePunctuation(finalTranscript);
+            console.log(finalTranscript);
+            const res = peg$parse(finalTranscript);
+            console.log(res);
+            res.execute();
+          }
         } finally {
           finalTranscript = "";
           player.SetVar("spokenText", "");
+          player.SetVar("sttIsActive", false);
         }
       }
     };
@@ -52,7 +77,7 @@ function startSTT() {
     recognition.onerror = function (event) {
       console.log(event);
       //This prevents stopping of the recognition
-      recognition.abort();
+      recognition.stop();
       if (player.GetVar("sttEnabled")) {
         recognition.start();
       }
@@ -81,6 +106,8 @@ function removePunctuation(finalTranscript) {
 
 function stopSTT() {
   console.log("Stop STT");
-  recognition.abort();
-  recognition = null;
+  if (recognition) {
+    recognition.stop();
+    recognition = null;
+  }
 }
