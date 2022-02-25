7fc4c9445e45bdb9e6a1424f4156a944f8eee7c8
proohit <direnc99@gmail.com>
2022-02-25 20:01:51 +0100

    chore: keep only articulate files

--- commandsGrammar/config.js
+++
@@ -1,5 +0,0 @@
-window.SPEECH_CONFIG = {
-  modelPath: "/commandsGrammar/vosk-model-small-de-0.15.tar.gz",
-  grammarPath: "/commandsGrammar/grammar.peggy",
-  wordlistPath: "/commandsGrammar/wordlist.js",
-};
